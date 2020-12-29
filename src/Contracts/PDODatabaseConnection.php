42d31fb5754217694dc1dedeaba17d36d3c92b03
Amir Salehi <isamirsalehi@gmail.com>
2020-12-29 14:47:05 +0330

    Connect and getConnection methods and their tests implemented

--- src/Contracts/PDODatabaseConnection.php
+++ src/Contracts/PDODatabaseConnection.php
@@ -2,22 +2,37 @@
 
 namespace Src\Contracts;
 
+use PDO;
+use PDOException;
+use Src\Exceptions\PDOConnectionException;
+
 class PDODatabaseConnection implements DatabaseConnectionInterface
 {
     protected $config;
+    protected $pdoConnection;
 
     public function __construct(array $config)
     {
         $this->config = $config;
-    }
-
-    public function getConnection()
-    {
-
+        $this->connect();
     }
 
     public function connect()
     {
-        // TODO: Implement connect() method.
+        try{
+            $this->pdoConnection = new PDO("mysql:host={$this->config['DB_HOST']};dbname={$this->config['DB_NAME']}", 
+            $this->config['DB_USER'], 
+            $this->config['DB_PASS']);
+        }catch(PDOException $e)
+        {
+            throw new PDOConnectionException($e->getMessage());
+        }
+
+        return $this;
+    }
+
+    public function getConnection()
+    {
+        return $this->getConnection;
     }
 }
--- tests/Unit/PDODatabaseConnectionTest.php
+++ tests/Unit/PDODatabaseConnectionTest.php
@@ -2,21 +2,32 @@
 
 namespace Tests\Unit;
 
+
 use PHPUnit\Framework\TestCase;
 use Src\Contracts\PDODatabaseConnection;
 use Src\Exceptions\WrongConfigDataException;
+use Src\Utilities\Helper;
 
 class PDODatabaseConnectionTest extends TestCase
 {
-    public function testIfConstructorGetArrayConfigData()
+    public function testIfGetConnectionReturnPDOObject()
     {
-//        $this->expectException(WrongConfigDataException::class);
-//
-//        $pdoDatabaseConnection = new PDODatabaseConnection();
+        $database_config = $this->getConfig();
+
+        $pdoDatabaseConnection = new PDODatabaseConnection($database_config);
+
+        $pdoConnection = $pdoDatabaseConnection->connect()->getConnection();
+
+
+        $this->assertIsObject($pdoConnection);
+        $this->assertEquals($pdoConnection['DB_HOST'], $database_config['DB_HOST']);
+        $this->assertEquals($pdoConnection['DB_NAME'], $database_config['DB_NAME']);
+        $this->assertEquals($pdoConnection['DB_USER'], $database_config['DB_USER']);
+        $this->assertEquals($pdoConnection['DB_PASS'], $database_config['DB_PASS']);
     }
 
     private function getConfig()
     {
-
+        return Helper::getConfig('database', 'mysql');
     }
 }
